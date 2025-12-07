b858d8c6daed3ea7bc65fae73974e8680d973734
YejinYoon <68535398+YejinY00n@users.noreply.github.com>
2025-12-07 21:26:39 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 94.23 ms, Memory: 97.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> participantMap = new HashMap<>();
+        
+        for(String part:participant) {
+            participantMap.put(part, participantMap.getOrDefault(part, 0)+1);
+        }
+        
+        for(String comp:completion) {
+            participantMap.put(comp, participantMap.get(comp)-1);
+        }
+        
+        for(String key:participantMap.keySet()) {
+            if(participantMap.get(key) > 0) {
+                return key;
+            }
+        }
+        
+        String answer = "";
+        return answer;
+    }
+}
